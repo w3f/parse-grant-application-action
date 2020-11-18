cf5444bd2e19e14cefd2787eeea1c97021c99cde
Marcin Górny <marcin@web3.foundation>
2020-11-18 15:23:30 +0100

    custom action, try 1

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict'
 
-const core = require('@actions/core')
-const { promises: fs } = require('fs')
+const core = require('@actions/core');
+const { promises: fs } = require('fs');
 
 const main = async () => {
-  const path = core.getInput('path')
-  const content = await fs.readFile(path, 'utf8')
-  core.setOutput('content', content)
+  const path = core.getInput('path');
+  const content = await fs.readFile(path, 'utf8');
+
+  const regex_project = /(?<=\*\*Project:\*\* ).*/g;
+  const regex_proposer = /(?<=\*\*Proposer:\*\* ).*/g;
+
+  const project_name = content.match(regex_project)[0];
+  const proposer = content.match(regex_proposer)[0];
+  core.setOutput('project_name', project_name);
+  core.setOutput('proposer', proposer);
 }
 
 main().catch(err => core.setFailed(err.message))
