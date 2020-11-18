fc10f7af106235fa424461c35b80c937761c9392
Marcin Górny <marcin@web3.foundation>
2020-11-18 16:39:27 +0100

    attempt 3

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ const main = async () => {
 
   const regex_project = /(?<=\*\*Project:\*\* ).*/g;
   const regex_proposer = /(?<=\*\*Proposer:\*\* ).*/g;
+  const regex_total_cost = /(?<=\*\*Total Costs:\*\* ).*(?= BTC)/g;
 
   const project_name = content.match(regex_project)[0];
   const proposer = content.match(regex_proposer)[0];
+  const total_cost = content.match(regex_total_cost)[0];
   core.setOutput('project_name', project_name);
-  core.setOutput('proposer', proposer);
+  core.setOutput('proposer', proposer)
+  core.setOutput('total_cost', total_cost)
 }
 
 main().catch(err => core.setFailed(err.message))
